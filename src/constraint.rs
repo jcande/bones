b1b1cbfe1cfb29e4f4460e3d13b89f9a12da8386
jsc <jsc+github@demigods.org>
2019-12-30 18:13:08 -0800

    This should have been part of the previous commit.

--- src/constraint.rs
+++ src/constraint.rs
@@ -17,127 +17,183 @@ pub struct Row<'a> {
     // and then we need to insert a blank tile to handle it.
     set: &'a TileSet,
     row: Vec<TileCloud<'a>>,
+    border: TileRef,
 }
 
 impl<'a> Row<'a> {
-    pub fn new(set: &'a TileSet, border: &Tile, board: Vec<TileRef>) -> Self {
+    pub fn new(set: &'a TileSet, border: &Tile, board: &Vec<TileRef>) -> Self {
         let both_fronts = 2; // west + east
         let mut row: Vec<TileCloud> = Vec::with_capacity(board.len() + both_fronts);
+        let border_ref = *set.get(border).expect("border must be contained in tileset");
 
-        let row = row;
+        // The main idea is that we may or may not use the border clouds. They are only added in
+        // case the machine expands. That leaves the loop where we generate the successor cloud
+        // based on the current row of tiles.
+
+        // west
+        {
+            let cloud = set.matches_tile(border, Direction::East);
+            let cloud = TileCloud::new(set, cloud, TileCloudConf::Prefer(border_ref));
+            row.push(cloud);
+        }
+
+        for r in board.iter() {
+            let cloud = set.matches_ref(r, Direction::South);
+            let cloud = TileCloud::new(set, cloud, TileCloudConf::Avoid(border_ref));
+            row.push(cloud);
+        }
+
+        // east
+        {
+            let cloud = set.matches_tile(border, Direction::West);
+            let cloud = TileCloud::new(set, cloud, TileCloudConf::Prefer(border_ref));
+            row.push(cloud);
+        }
+
         Self {
             set: set,
             row: row,
+            border: border_ref,
         }
     }
 
-    pub fn push(&mut self, cloud: TileCloud<'a>) {
-        if self.row.is_empty() {
-            // XXX push western cloud
-        }
-        self.row.push(cloud);
-    }
+    pub fn to_vec(mut self) -> Option<Vec<TileRef>> {
+        for i in 0..self.row.len() {
+            println!("everything referring to cloud {}", i);
 
-    pub fn to_vec(self) -> Option<Vec<TileRef>> {
-        // XXX push eastern cloud
+            if i > 0 {
+                // westward
+                /*
+                let pred  = &    self.row[i-1];
+                let cloud = &mut self.row[i];
+                */
+                let (earlier, later) = self.row[i-1..i+1].split_at_mut(1);
+                let pred = &earlier[0];
+                let cloud = &mut later[0];
+
+                if !cloud.constrain(pred, &Orientation::West) {
+println!("to_vec: {}, westward!", i);
+                    return None;
+                }
+            }
+
+            if i < (self.row.len() - 1) {
+                // eastward
+                /*
+                let succ  = &    self.row[i+1];
+                let cloud = &mut self.row[i];
+                */
+                let (earlier, later) = self.row[i..i+2].split_at_mut(1);
+                let cloud = &mut earlier[0];
+                let succ = &later[0];
+                if !cloud.constrain(succ, &Orientation::East) {
+println!("to_vec: {}, eastward! {:?}", i, succ);
+                    return None;
+                }
+            }
+        }
+
+        let mut next: Vec<TileRef> = self.row.iter().map(|cloud| cloud.select()).collect();
+        if next[0] == self.border {
+            next.remove(0);
+        }
+        if next[next.len() - 1] == self.border {
+            next.pop();
+        }
 
         /*
-                {
-                    /*
-                    let pred  = &    next[i-1];
-                    let cloud = &mut next[i];
-                    */
-                    let (earlier, later) = next[i-1..i+1].split_at_mut(1);
-                    let pred = &earlier[0];
-                    let cloud = &mut later[0];
-                    cloud.constrain(&self.from_ref, &Orientation::Backward, pred);
-                }
-
-                {
-                    /*
-                    let succ  = &    next[i+1];
-                    let cloud = &mut next[i];
-                    */
-                    let (earlier, later) = next[i..i+2].split_at_mut(1);
-                    let cloud = &mut earlier[0];
-                    let succ = &later[0];
-                    cloud.constrain(&self.from_ref, &Orientation::Forward,  succ);
-                }
+        let next = self.row.iter().enumerate()
+            .map(|(i, cloud)| (i, cloud.select()))
+            // Remove the border pieces if they are the expected border pieces. This is to
+            // prevent us adding 2 tiles per step.
+            .filter(|(i, tile)| (*i == 0 || *i == (self.row.len() - 1)) &&
+                    *tile != self.border)
+            .map(|(_, tile)| tile)
+            .collect();
         */
 
-        None
+        println!("next: {:?}", next);
+
+        Some(next)
     }
+}
+
+#[derive(Debug)]
+pub enum TileCloudConf {
+    Prefer(TileRef),
+    Avoid(TileRef),
 }
 
 #[derive(Debug)]
 // XXX this probably needs a reference to TileSet otherwise a caller could confuse them and then
 // we would crash
 pub struct TileCloud<'a> {
-    set: &'a TileSet,
+    tiles: &'a TileSet,
     cloud: HashSet<TileRef>,
+    conf: TileCloudConf,
 }
 
 // XXX think of operations
 
 impl<'a> TileCloud<'a> {
-    pub fn new(tiles: &'a TileSet, initial: Vec<TileRef>) -> Self {
+    pub fn new(tiles: &'a TileSet, initial: Vec<TileRef>, conf: TileCloudConf) -> Self {
         // XXX may want to pre-calculate each pip as that is used a lot
         Self {
-            set: tiles,
+            tiles: tiles,
             cloud: initial.into_iter().collect(),
-        }
-    }
-
-    pub fn print_tiles(&self, tiles: &TileSet) {
-        for r in self.cloud.iter() {
-            println!("{}: {:?}", r, tiles[*r]);
+            conf: conf,
         }
     }
 
     pub fn positional_pips(&self, direction: &Direction) -> HashSet<Pip> {
         self.cloud.iter()
             .cloned()
-            .map(|ref r| self.set[*r].cardinal(direction))
+            .map(|ref r| self.tiles[*r].cardinal(direction))
             .collect()
     }
 
-   // pub fn constrain_pip(&mut self, tiles: &TileSet, direction: &Direction, values: &[Pip]) -> bool {
-    pub fn constrain_pip(&mut self, tiles: &TileSet, direction: &Direction, value: &Pip) -> bool {
-        // XXX maybe with_capacity(self.cloud.len())? We'll use more space but probably faster
-        let mut keep = HashSet::new();
-        for r in self.cloud.iter() {
-            if tiles[*r].cardinal(direction) == *value {
-                keep.insert(*r);
-            }
-        }
-        self.cloud = keep;
-
-        self.cloud.len() > 0
-    }
-
-    // TODO
+    // Orientation is where the other tilecloud is in relation to self
     pub fn constrain(&mut self,
-                     tiles: &TileSet,
-                     orientation: &Orientation,
-                     other: &TileCloud) -> bool {
+                     other: &TileCloud,
+                     orientation: &Orientation
+                     ) -> bool {
         // XXX maybe with_capacity(self.cloud.len())? We'll use more space but probably faster
         let mut keep = HashSet::new();
-        let (current, next) = match orientation {
-            Orientation::East => (Direction::East, Direction::West),
-            Orientation::West => (Direction::West, Direction::East),
-            _ => panic!("north/south constraints don't make sense in this context"),
-        };
+        if *orientation == Orientation::North ||
+            *orientation == Orientation::South {
+            panic!("north/south constraints don't make sense in this context");
+        }
+        let (current, next) = (orientation, -*orientation);
 
         let available_pips = other.positional_pips(&next);
         for r in self.cloud.iter() {
-            let pip = tiles[*r].cardinal(&current);
+            let pip = self.tiles[*r].cardinal(&current);
             if available_pips.contains(&pip) {
                 keep.insert(*r);
             }
         }
+        println!("was: {:?}", self.cloud);
         self.cloud = keep;
         println!("keeping: {:?}", self.cloud);
 
         self.cloud.len() > 0
+    }
+
+    pub fn select(&self) -> TileRef {
+        for r in self.cloud.iter() {
+            match self.conf {
+                TileCloudConf::Prefer(tile_ref) => if tile_ref == *r {
+                    println!("preference obtained");
+                    return *r;
+                },
+                TileCloudConf::Avoid(tile_ref) => if tile_ref != *r {
+                    println!("avoidance obtained");
+                    return *r;
+                },
+            }
+        }
+
+        println!("gotta take what we can get");
+        *self.cloud.iter().next().expect("TileCloud needs to have valid tiles before it can select one")
     }
 }
--- src/tiling.rs
+++ src/tiling.rs
@@ -1,15 +1,12 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::ops::Index;
+use std::ops::Neg;
 
 // or Bone!
 pub type Pip = usize;
 
-// XXX shouldn't use this as it is too vague
-pub enum Axis {
-    Latitude,
-    Longitude,
-}
-
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum Direction {
     North,
     East,
@@ -17,6 +14,19 @@ pub enum Direction {
     West,
 }
 pub type Orientation = Direction;
+
+impl Neg for Direction {
+    type Output = Direction;
+
+    fn neg(self) -> Self::Output {
+        match self {
+            Direction::North => Direction::South,
+            Direction::South => Direction::North,
+            Direction::East  => Direction::West,
+            Direction::West  => Direction::East,
+        }
+    }
+}
 
 // A constraint satisfaction problem (CSP) has these 3 components:
 // 1) set of variables
@@ -51,6 +61,7 @@ impl Tile {
         }
     }
 
+    #[inline]
     pub fn cardinal(&self, direction: &Direction) -> Pip {
         match direction {
             Direction::North => self.north,
@@ -62,8 +73,81 @@ impl Tile {
 }
 
 
+// If we have more than 4 billion then we'll have to bump it
 pub type TileRef = u32;
-pub type TileSet = HashMap<TileRef, Tile>;
-pub type RevTileSet = HashMap<Tile, TileRef>;
-pub type BoardState = Vec<Tile>;
-pub type BoardStateRef = Vec<TileRef>;
+
+#[derive(Debug)]
+pub struct TileSet {
+    set:    Vec<Tile>,
+    lookup: HashMap<Tile, TileRef>,
+}
+
+impl TileSet {
+    pub fn new(set: Vec<Tile>) -> Self {
+        let mut lookup = HashMap::new();
+        for (i, tile) in set.iter().enumerate() {
+            lookup.insert(*tile, i as TileRef);
+        }
+
+        TileSet {
+            set,
+            lookup,
+        }
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item=&Tile> {
+        self.set.iter()
+    }
+
+    pub fn get(&self, tile: &Tile) -> Option<&TileRef> {
+        self.lookup.get(tile)
+    }
+
+    // XXX make a type for the return value
+    // The orientation is relative to the provided tile. E.g., if we say West, then we look at
+    // the westernmost pip of the tile and find all eastern pips that match
+    pub fn matches_ref(&self,
+               tile_ref: &TileRef,
+               direction: Orientation)
+        -> Vec<TileRef> {
+        let tile = self.set[*tile_ref as usize];
+        self.matches_tile(&tile, direction)
+    }
+
+    pub fn matches_tile(&self,
+                        tile: &Tile,
+                        direction: Orientation)
+        -> Vec<TileRef> {
+        let (current, next) = (direction, -direction);
+
+        let pip = tile.cardinal(&current);
+        self.lookup.iter()
+            .filter(|(tile, _)| pip == tile.cardinal(&next))
+            .map(|(_, r)| *r)
+            .collect()
+    }
+
+    // The orientation is relative to the pip. In other words, orientation refers to where the
+    // pip is located within a tile.
+    pub fn matches_pip(&self,
+               pip: &Pip,
+               direction: Orientation)
+        -> Vec<TileRef> {
+        let next = -direction;
+
+        self.lookup.iter()
+            .filter(|(tile, _)| *pip == tile.cardinal(&next))
+            .map(|(_, r)| *r)
+            .collect()
+    }
+}
+
+// XXX how can I inline this to the main struct impl?
+impl Index<TileRef> for TileSet {
+    type Output = Tile;
+
+    #[inline]
+    fn index(&self, index: TileRef) -> &Self::Output {
+        self.set.get(index as usize).expect("Out of bounds access")
+    }
+}
